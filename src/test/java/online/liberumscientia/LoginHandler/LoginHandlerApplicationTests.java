b1a7cf8007d7b7c4b83d6714b6304a7785b25134
gxbxD <gabrielldangelo@gmail.com>
2024-09-11 19:33:46 -0300

    fix

--- src/test/java/online/liberumscientia/LoginHandler/LoginHandlerApplicationTests.java
+++ src/test/java/online/liberumscientia/LoginHandler/LoginHandlerApplicationTests.java
@@ -3,7 +3,9 @@ package online.liberumscientia.LoginHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import online.liberumscientia.loginhandler.LoginHandlerApplication;
+
+@SpringBootTest(classes = LoginHandlerApplication.class)
 class LoginHandlerApplicationTests {
 
 	@Test
