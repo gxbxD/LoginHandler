3cd1f2bc8c86c63bccb0ec8136461526f3634b19
gxbxD <gabrielldangelo@gmail.com>
2024-09-11 20:14:31 -0300

    .

--- src/test/java/online/liberumscientia/LoginHandler/LoginHandlerApplicationTests.java
+++ src/test/java/online/liberumscientia/LoginHandler/LoginHandlerApplicationTests.java
@@ -2,9 +2,11 @@ package online.liberumscientia.LoginHandler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import online.liberumscientia.loginhandler.LoginHandlerApplication;
 
+@ActiveProfiles("test")
 @SpringBootTest(classes = LoginHandlerApplication.class)
 class LoginHandlerApplicationTests {
 
