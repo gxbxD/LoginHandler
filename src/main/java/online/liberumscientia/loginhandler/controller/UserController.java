115d06f936c47f6dca165d0a44abf9fa6edc40c5
gxbxD <gabrielldangelo@gmail.com>
2024-09-19 23:54:28 -0300

    .

--- src/main/java/online/liberumscientia/loginhandler/controller/UserController.java
+++ src/main/java/online/liberumscientia/loginhandler/controller/UserController.java
@@ -1,5 +1,13 @@
 package online.liberumscientia.loginhandler.controller;
 
+import online.liberumscientia.loginhandler.service.AuthService;
+import online.liberumscientia.loginhandler.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
 @RestController
 public class UserController {
 
@@ -7,7 +15,7 @@ public class UserController {
         private AuthService authService;
 
         @PostMapping("/api/register")
-        public ResponseEntity<?>registerUser(@Valid @RequestBody UserDto userDto) {
+        public ResponseEntity<?>registerUser(@RequestBody UserDto userDto) {
             try {
                 authService.registerUser(userDto.getEmail(), userDto.getPassword());
                 return ResponseEntity.ok("Usuário Registrado com sucesso!");
