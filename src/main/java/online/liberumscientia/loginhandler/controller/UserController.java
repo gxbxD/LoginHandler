464d5246c80b61a797b1618e3b0ba5b1a36e3bfd
gxbxD <gabrielldangelo@gmail.com>
2024-09-19 23:48:06 -0300

    .

--- src/main/java/online/liberumscientia/loginhandler/controller/UserController.java
+++ src/main/java/online/liberumscientia/loginhandler/controller/UserController.java
@@ -1,35 +1,18 @@
 package online.liberumscientia.loginhandler.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import online.liberumscientia.loginhandler.dto.UserDto;
-import online.liberumscientia.loginhandler.model.User;
-import online.liberumscientia.loginhandler.repository.UserRepository;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 @RestController
-@RequestMapping("/api")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository; // Supondo que você tenha um repositório para usuários
+        @Autowired
+        private AuthService authService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody UserDto userDto) {
-        if (userRepository.existsByEmail(userDto.getEmail())) {
-            return ResponseEntity.badRequest().body("Email já registrado");
+        @PostMapping("/api/register")
+        public ResponseEntity<?>registerUser(@Valid @RequestBody UserDto userDto) {
+            try {
+                authService.registerUser(userDto.getEmail(), userDto.getPassword());
+                return ResponseEntity.ok("Usuário Registrado com sucesso!");
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body("Erro:" + e.getMessage());
+            }
         }
-        User user = new User();
-        user.setEmail(userDto.getEmail());
-        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
-        userRepository.save(user);
-        return ResponseEntity.ok("Usuário registrado com sucesso");
-    }
 }
-
